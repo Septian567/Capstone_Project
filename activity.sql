4d45bcf2e6135c3565dcf641ce87e9c3e31d495f
Septian Dika Maulana <99572215+Septian567@users.noreply.github.com>
2022-05-20 13:04:06 +0700

    Update activity.sql

--- activity.sql
+++ activity.sql
@@ -2,9 +2,9 @@ use fit_in;
 
 CREATE TABLE activity
 (
-    id     VARCHAR(100)          NOT NULL,
-    calories_needed   INT(100) NOT NULL DEFAULT 0,
-    calories_fulfilled  INT(100)   NOT NULL DEFAULT 0
+    id     VARCHAR(100) NOT NULL,
+    calories_needed	INT(100) NOT NULL DEFAULT 0,
+    calories_fulfilled  INT(100) NOT NULL DEFAULT 0
 ) ENGINE = InnoDB;
 
 ALTER TABLE activity
